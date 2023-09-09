6a080225405d44aeab557ac984a811c5cf3872c7
MarvinKatapult <ibingmarvin@gmail.com>
2023-09-09 21:59:24 +0200

    init

---
+++ sketch.js
@@ -0,0 +1,29 @@
+const STARTPOINT = -90;
+var a = STARTPOINT;
+
+function setup() {
+  createCanvas(1000, 700);
+  background(255);
+  angleMode(DEGREES);
+
+}
+
+
+
+function draw() {
+  var r = 200;
+  colorMode(HSB, 360 + STARTPOINT);
+
+  stroke(color(a, 270, 270));
+  strokeWeight(5);
+
+  
+  point(width/2 + cos(a) * r , height/2 + sin(a) * r);
+ 
+
+  if (a > (360 + STARTPOINT)) {
+    a = STARTPOINT;
+    background(255);
+  }
+  a += 3;
+}

71154343af3000169e34706c0f0f225d14ada4a1
MarvinKatapult <ibingmarvin@gmail.com>
2023-09-10 12:06:42 +0200

    filledCircle

--- sketch.js
+++ sketch.js
@@ -1,26 +1,36 @@
 const STARTPOINT = -90;
+const FULLCIRCLEDEGREES = 360 + STARTPOINT;
 var a = STARTPOINT;
+var points = [];
 
 function setup() {
   createCanvas(1000, 700);
-  background(255);
+  colorMode(HSB, FULLCIRCLEDEGREES);
+  background(FULLCIRCLEDEGREES);
   angleMode(DEGREES);
-
 }
 
 function draw() {
-  var r = 200;
-  colorMode(HSB, 360 - abs(STARTPOINT));
+  var r = 250;
+  var c = color(a, FULLCIRCLEDEGREES, FULLCIRCLEDEGREES);
 
-  stroke(color(a, 270, 270));
+  background(FULLCIRCLEDEGREES);
+  stroke(c);
+  fill(c)
   strokeWeight(5);
 
-  point(width/2 + cos(a) * r , height/2 + sin(a) * r);
+  points[points.length] = createVector(width/2 + cos(a) * r , height/2 + sin(a) * r);
  
-  if (a > (360 - abs(STARTPOINT))) {
+  if (a > (FULLCIRCLEDEGREES)) {
+    points = []
     a = STARTPOINT;
-    background(255);
   }
+  
+  beginShape();
+    for (var i = 0 ; i < points.length; i++) {
+      vertex(points[i].x, points[i].y)
+    }
+  endShape(CLOSE);
 
-  a += 3;
+  a += 1;
 }
